46734b1010abae94d1febb30bee03eef7ac7bb37
chihebHS <131190457+chihebHS@users.noreply.github.com>
2026-07-17 06:57:05 +0100

    Delete faAI_and_OrderAI_module directory

--- faAI_and_OrderAI_module/utils.py
+++
@@ -1,8 +0,0 @@
-import unicodedata
-
-
-def normalize(text: str) -> str:
-    return ''.join(
-        c for c in unicodedata.normalize('NFD', text)
-        if unicodedata.category(c) != 'Mn'
-    ).lower()
