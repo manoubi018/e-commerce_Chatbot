a9afa803a4e88bb27cbb091d8a1ef4ce69e773c2
chihebHS <131190457+chihebHS@users.noreply.github.com>
2026-07-17 06:56:50 +0100

    Delete endpoint_md_identificationIMG directory

--- endpoint_md_identificationIMG/app/database.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine, Column, String, Float, JSON
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-engine = create_engine("sqlite:///app.db")
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
